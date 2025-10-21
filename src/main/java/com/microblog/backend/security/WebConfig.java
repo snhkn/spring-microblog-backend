484581dbdabfc76c2b7c824b97f4bed8ab094877
snhkn <senihaakin@gmail.com>
2025-10-21 13:41:19 +0300

    Fix the CORS issue

--- src/main/java/com/microblog/backend/security/WebConfig.java
+++ src/main/java/com/microblog/backend/security/WebConfig.java
@@ -15,7 +15,9 @@ public class WebConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowCredentials(true)
+                        .allowedHeaders("*")
+                        .exposedHeaders("Set-Cookie");
             }
         };
     }
